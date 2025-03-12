b84c73eb7eba9092d45375105ca3f42f82de7f67
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-03-11 23:46:16 -0400

    Squall golem is completly functional

--- src/main/java/net/firefoxsalesman/dungeonsmobs/client/models/golem/SquallGolemModel.java
+++ src/main/java/net/firefoxsalesman/dungeonsmobs/client/models/golem/SquallGolemModel.java
@@ -3,7 +3,6 @@ package net.firefoxsalesman.dungeonsmobs.client.models.golem;
 import net.firefoxsalesman.dungeonsmobs.DungeonsMobs;
 import net.firefoxsalesman.dungeonsmobs.entity.golem.SquallGolemEntity;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
 import software.bernie.geckolib.core.animation.AnimationState;
@@ -36,26 +35,25 @@ public class SquallGolemModel extends GeoModel<SquallGolemEntity> {
 		CoreGeoBone head = this.getAnimationProcessor().getBone("head");
 		CoreGeoBone eye = this.getAnimationProcessor().getBone("head2");
 		CoreGeoBone eyeBrow = this.getAnimationProcessor().getBone("head3");
-		if (head != null) {
-			EntityModelData extraData = customPredicate.getData(DataTickets.ENTITY_MODEL_DATA);
-			head.setRotX(extraData.headPitch() * Mth.DEG_TO_RAD);
-			head.setRotY(extraData.netHeadYaw() * Mth.DEG_TO_RAD);
-			eye.setPosY(Math.max(Math.min(extraData.headPitch() / 80, 0.15F), -0.2F));
-			eyeBrow.setPosY(Math.max(Math.min(extraData.headPitch() / 80, 0.15F), -0.2F));
-		}
-		// if (extraData.headPitch != 0 || extraData.netHeadYaw != 0) {
-		// eye.setPositionX((float) Math.max(Math.min(
-		// (extraData.netHeadYaw / 80) + Math.sin(eye.getPositionX() * Math.PI / 180F),
-		// 1),
-		// -1));
-		// head.setRotationX(head.getRotationX() + (extraData.headPitch * ((float)
-		// Math.PI / 180F)));
-		// head.setRotationZ(head.getRotationZ() + (extraData.netHeadYaw * ((float)
-		// Math.PI / 180F)));
-		// eye.setPositionY(Math.max(Math.min(extraData.headPitch / 80, 0.15F), -0.2F));
-		// eyeBrow.setPositionY(Math.max(Math.min(extraData.headPitch / 80, 0.15F),
+		EntityModelData extraData = customPredicate.getData(DataTickets.ENTITY_MODEL_DATA);
+		// if (head != null) {
+		// EntityModelData extraData =
+		// customPredicate.getData(DataTickets.ENTITY_MODEL_DATA);
+		// head.setRotX(extraData.headPitch() * Mth.DEG_TO_RAD);
+		// head.setRotY(extraData.netHeadYaw() * Mth.DEG_TO_RAD);
+		// eye.setPosY(Math.max(Math.min(extraData.headPitch() / 80, 0.15F), -0.2F));
+		// eyeBrow.setPosY(Math.max(Math.min(extraData.headPitch() / 80, 0.15F),
 		// -0.2F));
 		// }
+		if (extraData.headPitch() != 0 || extraData.netHeadYaw() != 0) {
+			eye.setPosX((float) Math.max(Math.min(
+					(extraData.netHeadYaw() / 80) + Math.sin(eye.getPosX() * Math.PI / 180F), 1),
+					-1));
+			head.setRotX(head.getRotX() + (extraData.headPitch() * ((float) Math.PI / 180F)));
+			head.setRotZ(head.getRotZ() + (extraData.netHeadYaw() * ((float) Math.PI / 180F)));
+			eye.setPosY(Math.max(Math.min(extraData.headPitch() / 80, 0.15F), -0.2F));
+			eyeBrow.setPosY(Math.max(Math.min(extraData.headPitch() / 80, 0.15F),0.2F));
+		}
 	}
 
 }
