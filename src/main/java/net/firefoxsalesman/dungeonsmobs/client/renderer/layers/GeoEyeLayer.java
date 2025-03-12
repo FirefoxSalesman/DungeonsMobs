ddcadcb4ed4bd9fea2f861de1ce080c1bf812fba
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-03-12 15:27:08 -0400

    Did something to eye layer rendering. Not sure if it's an improvement

--- src/main/java/net/firefoxsalesman/dungeonsmobs/client/renderer/layers/GeoEyeLayer.java
+++ src/main/java/net/firefoxsalesman/dungeonsmobs/client/renderer/layers/GeoEyeLayer.java
@@ -1,7 +1,5 @@
 package net.firefoxsalesman.dungeonsmobs.client.renderer.layers;
 
-import java.util.Random;
-
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 
@@ -9,6 +7,7 @@ import net.firefoxsalesman.dungeonsmobs.DungeonsMobs;
 import net.firefoxsalesman.dungeonsmobs.entity.ender.BlastlingEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
@@ -22,13 +21,10 @@ import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 public class GeoEyeLayer<T extends LivingEntity & GeoEntity> extends AutoGlowingGeoLayer<T> {
 
 	public ResourceLocation textureLocation;
-	// private Random random;
-	// int tick;
 
 	public GeoEyeLayer(GeoRenderer<T> endermanReplacementRenderer, ResourceLocation textureLocation) {
 		super(endermanReplacementRenderer);
 		this.textureLocation = textureLocation;
-		// random = new Random();
 	}
 
 	@Override
@@ -36,18 +32,40 @@ public class GeoEyeLayer<T extends LivingEntity & GeoEntity> extends AutoGlowing
 		return textureLocation;
 	}
 
+	// @Override
+	// public void render(PoseStack poseStack, T animatable, BakedGeoModel
+	// bakedModel, RenderType renderType,
+	// MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int
+	// packedLight,
+	// int packedOverlay) {
+	// if (animatable instanceof BlastlingEntity) {
+	// textureLocation = new ResourceLocation(DungeonsMobs.MOD_ID,
+	// "textures/entity/ender/blastling"
+	// + (1 + ((int) ((BlastlingEntity) animatable).flameTicks) % 3)
+	// + "_eyes.png");
+	// }
+	// super.render(poseStack, animatable, bakedModel, renderType, bufferSource,
+	// buffer, partialTick,
+	// packedLight,
+	// packedOverlay);
+	// }
 	@Override
 	public void render(PoseStack poseStack, T animatable, BakedGeoModel bakedModel, RenderType renderType,
 			MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight,
 			int packedOverlay) {
+		RenderType emmissiveRenderType = getRenderType(animatable);
 		if (animatable instanceof BlastlingEntity) {
 			textureLocation = new ResourceLocation(DungeonsMobs.MOD_ID, "textures/entity/ender/blastling"
 					+ (1 + ((int) ((BlastlingEntity) animatable).flameTicks) % 3)
 					+ "_eyes.png");
+			getRenderer().reRender(bakedModel, poseStack, bufferSource, animatable, emmissiveRenderType,
+					bufferSource.getBuffer(emmissiveRenderType), partialTick, 15728640,
+					OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+		} else {
+			getRenderer().reRender(bakedModel, poseStack, bufferSource, animatable, emmissiveRenderType,
+					bufferSource.getBuffer(emmissiveRenderType), partialTick, 15728640,
+					OverlayTexture.NO_OVERLAY, .8F, .8F, .8F, 1.0F);
 		}
-		super.render(poseStack, animatable, bakedModel, renderType, bufferSource, buffer, partialTick,
-				packedLight,
-				packedOverlay);
 	}
 
 	@Override
