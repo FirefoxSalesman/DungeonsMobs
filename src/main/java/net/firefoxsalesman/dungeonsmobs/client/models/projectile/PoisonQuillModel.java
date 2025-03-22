15cb6c2864562ed6a400e1d5817067f756336e15
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-03-22 10:38:32 -0400

    Fixed poison quill rotation

--- src/main/java/net/firefoxsalesman/dungeonsmobs/client/models/projectile/PoisonQuillModel.java
+++ src/main/java/net/firefoxsalesman/dungeonsmobs/client/models/projectile/PoisonQuillModel.java
@@ -3,8 +3,6 @@ package net.firefoxsalesman.dungeonsmobs.client.models.projectile;
 import net.firefoxsalesman.dungeonsmobs.DungeonsMobs;
 import net.firefoxsalesman.dungeonsmobs.entity.projectiles.PoisonQuillEntity;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
-import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
 
 public class PoisonQuillModel extends GeoModel<PoisonQuillEntity> {
@@ -24,14 +22,5 @@ public class PoisonQuillModel extends GeoModel<PoisonQuillEntity> {
 		return new ResourceLocation(DungeonsMobs.MOD_ID,
 				entity.isKelp() ? "textures/entity/projectile/water_poison_quill.png"
 						: "textures/entity/projectile/poison_quill.png");
-	}
-
-	@Override
-	public void setCustomAnimations(PoisonQuillEntity entity, long uniqueID,
-			AnimationState<PoisonQuillEntity> customPredicate) {
-		super.setCustomAnimations(entity, uniqueID, customPredicate);
-		CoreGeoBone everything = this.getAnimationProcessor().getBone("everything");
-
-		everything.setRotY(-1.5708F);
 	}
 }
