1ad9f696ec3b6e8be5cbe269394e6e083d970b52
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-05-02 21:22:01 -0400

    Raid entries can now be configured

--- src/main/java/net/firefoxsalesman/dungeonsmobs/config/DungeonsMobsConfig.java
+++ src/main/java/net/firefoxsalesman/dungeonsmobs/config/DungeonsMobsConfig.java
@@ -8,12 +8,45 @@ public class DungeonsMobsConfig {
 	public static class Common {
 		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_ICY_CREEPER_GRIEFING;
 
+		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_MOUNTAINEERS_IN_RAIDS;
+		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_ROYAL_GUARDS_IN_RAIDS;
+		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_GEOMANCERS_IN_RAIDS;
+		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_MAGES_IN_RAIDS;
+		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_WINDCALLERS_IN_RAIDS;
+		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_SQUALL_GOLEMS_IN_RAIDS;
+		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_REDSTONE_GOLEMS_IN_RAIDS;
+
 		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_STRONGER_HUSKS;
 		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_RANGED_SPIDERS;
 		public final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_HOSTILE_MOOSHROOMS;
 		public static ForgeConfigSpec.ConfigValue<Boolean> ENABLE_ITEM_TAB;
 
 		public Common(ForgeConfigSpec.Builder builder) {
+			// RAID CONFIGURATION
+			builder.comment("Raid Configuration").push("raid_configuration");
+			ENABLE_MOUNTAINEERS_IN_RAIDS = builder
+					.comment("Enable the addition of Mountaineers to raids. [true / false]")
+					.define("enableMountaineersInRaids", false);
+			ENABLE_ROYAL_GUARDS_IN_RAIDS = builder
+					.comment("Enable the addition of Royal Guards to raids. [true / false]")
+					.define("enableRoyalGuardsInRaids", false);
+			ENABLE_GEOMANCERS_IN_RAIDS = builder
+					.comment("Enable the addition of Geomancers to raids. [true / false]")
+					.define("enableGeomancersInRaids", true);
+			ENABLE_MAGES_IN_RAIDS = builder
+					.comment("Enable the addition of Mages to raids. [true / false]")
+					.define("enableMagesInRaids", false);
+			ENABLE_WINDCALLERS_IN_RAIDS = builder
+					.comment("Enable the addition of Windcallers to raids. [true / false]")
+					.define("enableWindcallersInRaids", false);
+			ENABLE_SQUALL_GOLEMS_IN_RAIDS = builder
+					.comment("Enable the addition of Squall Golems to raids. [true / false]")
+					.define("enableSquallGolemsInRaids", true);
+			ENABLE_REDSTONE_GOLEMS_IN_RAIDS = builder
+					.comment("Enable the addition of Redstone Golems to raids. [true / false]")
+					.define("enableRedstoneGolemsInRaids", true);
+			builder.pop();
+
 			// MOB CONFIGURATION
 			builder.comment("Mob Configuration").push("mob_configuration");
 			ENABLE_ICY_CREEPER_GRIEFING = builder
--- src/main/java/net/firefoxsalesman/dungeonsmobs/worldgen/RaidEntries.java
+++ src/main/java/net/firefoxsalesman/dungeonsmobs/worldgen/RaidEntries.java
@@ -1,5 +1,6 @@
 package net.firefoxsalesman.dungeonsmobs.worldgen;
 
+import net.firefoxsalesman.dungeonsmobs.config.DungeonsMobsConfig;
 import net.firefoxsalesman.dungeonsmobs.entity.ModEntities;
 import net.minecraft.world.entity.raid.Raid;
 
@@ -14,23 +15,41 @@ public class RaidEntries {
 		 * RAVAGER(EntityType.RAVAGER, new int[]{0, 0, 0, 1, 0, 1, 0, 2});
 		 */
 
-		Raid.RaiderType.create("mountaineer", ModEntities.MOUNTAINEER.get(),
-				new int[] { 0, 0, 2, 0, 1, 4, 2, 5 });
+		// WARRIOR
+		if (DungeonsMobsConfig.COMMON.ENABLE_MOUNTAINEERS_IN_RAIDS.get()) {
+			Raid.RaiderType.create("mountaineer", ModEntities.MOUNTAINEER.get(),
+					new int[] { 0, 0, 2, 0, 1, 4, 2, 5 });
+		}
 
-		Raid.RaiderType.create("royal_guard", ModEntities.ROYAL_GUARD.get(),
-				new int[] { 0, 0, 1, 0, 0, 2, 1, 2 });
+		if (DungeonsMobsConfig.COMMON.ENABLE_ROYAL_GUARDS_IN_RAIDS.get()) {
+			Raid.RaiderType.create("royal_guard", ModEntities.ROYAL_GUARD.get(),
+					new int[] { 0, 0, 1, 0, 0, 2, 1, 2 });
+		}
 
-		Raid.RaiderType.create("geomancer", ModEntities.GEOMANCER.get(),
-				new int[] { 0, 0, 0, 0, 0, 1, 1, 2 });
+		// SPELLCASTER
+		if (DungeonsMobsConfig.COMMON.ENABLE_GEOMANCERS_IN_RAIDS.get()) {
+			Raid.RaiderType.create("geomancer", ModEntities.GEOMANCER.get(),
+					new int[] { 0, 0, 0, 0, 0, 1, 1, 2 });
+		}
 
-		Raid.RaiderType.create("mage", ModEntities.MAGE.get(), new int[] { 0, 0, 1, 0, 0, 1, 0, 2 });
+		if (DungeonsMobsConfig.COMMON.ENABLE_MAGES_IN_RAIDS.get()) {
+			Raid.RaiderType.create("mage", ModEntities.MAGE.get(), new int[] { 0, 0, 1, 0, 0, 1, 0, 2 });
+		}
 
-		Raid.RaiderType.create("windcaller", ModEntities.WINDCALLER.get(),
-				new int[] { 0, 0, 0, 0, 0, 1, 1, 2 });
-		Raid.RaiderType.create("squall_golem", ModEntities.SQUALL_GOLEM.get(),
-				new int[] { 0, 0, 0, 1, 0, 1, 0, 2 });
+		if (DungeonsMobsConfig.COMMON.ENABLE_WINDCALLERS_IN_RAIDS.get()) {
+			Raid.RaiderType.create("windcaller", ModEntities.WINDCALLER.get(),
+					new int[] { 0, 0, 0, 0, 0, 1, 1, 2 });
+		}
 
-		Raid.RaiderType.create("redstone_golem", ModEntities.REDSTONE_GOLEM.get(),
-				new int[] { 0, 0, 0, 0, 0, 0, 0, 1 });
+		// BEAST / GOLEM
+		if (DungeonsMobsConfig.COMMON.ENABLE_SQUALL_GOLEMS_IN_RAIDS.get()) {
+			Raid.RaiderType.create("squall_golem", ModEntities.SQUALL_GOLEM.get(),
+					new int[] { 0, 0, 0, 1, 0, 1, 0, 2 });
+		}
+
+		if (DungeonsMobsConfig.COMMON.ENABLE_REDSTONE_GOLEMS_IN_RAIDS.get()) {
+			Raid.RaiderType.create("redstone_golem", ModEntities.REDSTONE_GOLEM.get(),
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 1 });
+		}
 	}
 }
