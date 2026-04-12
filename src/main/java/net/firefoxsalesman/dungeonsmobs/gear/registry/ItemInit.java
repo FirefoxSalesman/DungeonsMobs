7c9c5d0f18acf75a25c243233a353e79758dc86a
Carson Holscher <firefoxsalesman2@protonmail.com>
2026-04-12 12:40:32 -0400

    Added a few bows

--- src/main/java/net/firefoxsalesman/dungeonsmobs/gear/registry/ItemInit.java
+++ src/main/java/net/firefoxsalesman/dungeonsmobs/gear/registry/ItemInit.java
@@ -4,6 +4,7 @@ import net.firefoxsalesman.dungeonsmobs.gear.items.melee.ShearsGear;
 import net.firefoxsalesman.dungeonsmobs.gear.items.melee.StaffGear;
 import net.firefoxsalesman.dungeonsmobs.gear.utilities.GeneralHelper;
 import net.firefoxsalesman.dungeonsmobs.lib.items.gearconfig.AxeGear;
+import net.firefoxsalesman.dungeonsmobs.lib.items.gearconfig.BowGear;
 import net.firefoxsalesman.dungeonsmobs.lib.items.gearconfig.MeleeGear;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -21,18 +22,19 @@ import java.util.function.Supplier;
 
 public class ItemInit {
 	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-	public static final Item.Properties MELEE_WEAPON_PROPERTIES = new Item.Properties();
 	public static final Map<ResourceLocation, RegistryObject<Item>> MELEE_WEAPONS = new HashMap<>();
-	private static Supplier<Item> meleeSupplier = () -> new MeleeGear(MELEE_WEAPON_PROPERTIES);
-	private static Supplier<Item> staffSupplier = () -> new StaffGear(MELEE_WEAPON_PROPERTIES);
-	private static Supplier<Item> axeSupplier = () -> new AxeGear(MELEE_WEAPON_PROPERTIES);
+	public static final Map<ResourceLocation, RegistryObject<Item>> RANGED_WEAPONS = new HashMap<>();
+	private static Supplier<Item> meleeSupplier = () -> new MeleeGear(new Item.Properties());
+	private static Supplier<Item> staffSupplier = () -> new StaffGear(new Item.Properties());
+	private static Supplier<Item> axeSupplier = () -> new AxeGear(new Item.Properties());
+	private static Supplier<Item> bowSupplier = () -> new BowGear(new Item.Properties());
 
 	// DPS 9.6 (19.2), crits once per 2.5 (1.25) seconds
 	public static final RegistryObject<Item> DAGGER = registerMeleeWeapon("dagger", meleeSupplier);
 	public static final RegistryObject<Item> FANG_OF_FROST = registerMeleeWeapon("fang_of_frost", meleeSupplier);
 	public static final RegistryObject<Item> MOON_DAGGER = registerMeleeWeapon("moon_dagger", meleeSupplier);
 	public static final RegistryObject<Item> SHEAR_DAGGER = registerMeleeWeapon("shear_dagger",
-			() -> new ShearsGear(MELEE_WEAPON_PROPERTIES));
+			() -> new ShearsGear(new Item.Properties()));
 	// DPS 10 (20), crits once per 3 (1.5) seconds
 	public static final RegistryObject<Item> SICKLE = registerMeleeWeapon("sickle", meleeSupplier);
 	public static final RegistryObject<Item> NIGHTMARES_BITE = registerMeleeWeapon("nightmares_bite",
@@ -145,9 +147,26 @@ public class ItemInit {
 	public static final RegistryObject<Item> ENCRUSTED_ANCHOR = registerMeleeWeapon("encrusted_anchor",
 			meleeSupplier);
 
+	public static final RegistryObject<Item> BONEBOW = registerRangedWeapon("bonebow", bowSupplier);
+	public static final RegistryObject<Item> TWIN_BOW = registerRangedWeapon("twin_bow", bowSupplier);
+	public static final RegistryObject<Item> HAUNTED_BOW = registerRangedWeapon("haunted_bow", bowSupplier);
+
+	public static final RegistryObject<Item> SOUL_BOW = registerRangedWeapon("soul_bow", bowSupplier);
+	// public static final RegistryObject<Item> BOW_OF_LOST_SOULS =
+	// registerRangedWeapon("bow_of_lost_souls",
+	// bowSupplier);
+	public static final RegistryObject<Item> NOCTURNAL_BOW = registerRangedWeapon("nocturnal_bow", bowSupplier);
+	public static final RegistryObject<Item> SHIVERING_BOW = registerRangedWeapon("shivering_bow", bowSupplier);
+
 	private static RegistryObject<Item> registerMeleeWeapon(String meleeWeaponId, Supplier<Item> itemSupplier) {
 		RegistryObject<Item> register = ITEMS.register(meleeWeaponId, itemSupplier);
 		MELEE_WEAPONS.put(GeneralHelper.modLoc(meleeWeaponId), register);
+		return register;
+	}
+
+	private static RegistryObject<Item> registerRangedWeapon(String rangedWeaponId, Supplier<Item> itemSupplier) {
+		RegistryObject<Item> register = ITEMS.register(rangedWeaponId, itemSupplier);
+		RANGED_WEAPONS.put(GeneralHelper.modLoc(rangedWeaponId), register);
 		return register;
 	}
 
