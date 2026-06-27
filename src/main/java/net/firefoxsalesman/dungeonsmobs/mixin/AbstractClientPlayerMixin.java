9b930052d0423f210e78a1cec68eacd33c4f0b3c
Carson Holscher <firefoxsalesman2@protonmail.com>
2026-06-27 11:55:54 -0400

    I fixed it.

--- src/main/java/net/firefoxsalesman/dungeonsmobs/mixin/AbstractClientPlayerMixin.java
+++ src/main/java/net/firefoxsalesman/dungeonsmobs/mixin/AbstractClientPlayerMixin.java
@@ -1,18 +1,21 @@
 package net.firefoxsalesman.dungeonsmobs.mixin;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AbstractClientPlayer.class)
 public class AbstractClientPlayerMixin {
 
-	@Redirect(method = "getFieldOfViewModifier", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
-	private boolean handleIsBow(ItemStack instance, Item pItem) {
+	@WrapOperation(method = "getFieldOfViewModifier", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+	private boolean handleIsBow(ItemStack instance, Item pItem, Operation<Boolean> original) {
 		return instance.getItem() instanceof BowItem;
 	}
 }
