72130f7980c4b4e02826b190343abd067ad01689
Carson Holscher <firefoxsalesman2@protonmail.com>
2026-09-20 17:11:50 -0400

    Boss bar is mostly fine

--- src/main/java/net/firefoxsalesman/dungeonsmobs/client/renderer/BossBarRenderer.java
+++ src/main/java/net/firefoxsalesman/dungeonsmobs/client/renderer/BossBarRenderer.java
@@ -51,6 +51,7 @@ public class BossBarRenderer {
 		int i = (int) (percent * 182.0F);
 		int pX2 = pX + 9;
 		int pY2 = pY + 4;
+		guiGraphics.blit(TEXTURE, pX2, pY2, 0, 17, 182, 5, 182, 128);
 		guiGraphics.blit(TEXTURE, pX2, pY2, 0, 9, i, 5, 182, 128);
 		if (i > 0)
 			guiGraphics.blit(TEXTURE, pX2, pY2, 0, 1, 182, 5, 182, 128);
